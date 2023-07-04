08873010cc08cbff35ad86ae70981d36a29c07b7
kuma <40682653+ohuton55@users.noreply.github.com>
2023-07-05 08:44:27 +0900

    ブラウザのエラーがFTP伝送しても直らない
    履歴を削除して再実行するとうまくいくかも？

--- js-scene/SceneTitle.js
+++ js-scene/SceneTitle.js
@@ -1,28 +1,11 @@
 'use strict';
 
 class SceneTitle {
-	// 開始
-//	static start(gameData, userData) {
+
 	static start() {
-			let element = document.getElementById('test');
-			element.insertAdjacentHTML('afterend', '<p>SceneTitle</p>');
-			
-			GameAnim.add(this.anim.bind(this, gameData));  // アニメの登録
+
+	console.log("test");	
+
 	}
-	//------------------------------------------------------------
-	// アニメーション
-	static anim() {
-	
-//	const textArr = [		// 文字列配列
-//		'OheyaQuest',
-//		'Please Click!',
-//		'Created by Yuka Kumai',
-//		'Sound by Maoudamashii'
-//	];
-	
-	// 描画準備
-//	const layerId = 2;
-	
-	console.log("anin test");
-	}
+
 }
