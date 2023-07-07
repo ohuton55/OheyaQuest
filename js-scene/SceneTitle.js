4f90a5fd243e8146fb155b84e65c2cf183e99ab3
kuma <40682653+ohuton55@users.noreply.github.com>
2023-07-07 19:44:26 +0900

    タッチイベントがようやく正しく動作しているのを確認できた！

--- js-scene/SceneTitle.js
+++ js-scene/SceneTitle.js
@@ -6,9 +6,6 @@ class SceneTitle {
 
 		console.log("test");	
 		console.log(document.getElementById("myid"));
-		document.getElementById("text-button").onclick = function(){
-			document.getElementById("text").innerHTML = "CLICKED!";
-		} 
 	}
 
 }
--- main.js
+++ main.js
@@ -4,4 +4,38 @@ window.onload = function() {
 	eruda.init();
 	console.log('got it!');
 	SceneTitle.start();
+	
+	var result = Boolean( window.TouchEvent );
+	console.log(result);
+	
+	var result2 = Boolean(document.addEventListener);
+	console.log(result2);
+	
+//	document.addEventListener("touchstart", TouchEventFunc);
+//	document.addEventListener("touchmove", TouchEventFunc);
+//	document.addEventListener("touchend", TouchEventFunc);
+	document.getElementById("text-button").addEventListener("touchstart", TouchTextButton);
+	document.getElementById("text-button").addEventListener("touchmove", TouchEventFunc);
+//	document.getElementById("text-button").addEventListener("touchend", TouchEventFunc);
 };
+
+function TouchEventFunc(e) {
+	// get the obj for TouchList
+	var touch_list = e.changedTouches;
+	
+	// Accessing contains in order
+	var num = touch_list.length;
+	for(let i = 0; i < num; i++) {
+		// get the obj for Touch
+		var touch = touch_list[i];
+		// test output
+		console.log(touch);
+	}
+}
+
+function TouchTextButton(e) {
+	
+	const x  = e.touches[0].pageX;
+	const y = e.touches[0].pageY;
+			document.getElementById("text").innerHTML = "X:" + x + ". Y:" + y;
+		} 
