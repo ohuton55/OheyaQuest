96122b51f6f7eb80885697289a0224bfdec0be90
kuma <40682653+ohuton55@users.noreply.github.com>
2023-07-10 12:36:09 +0900

    CSSルールを取得する関数を追加したけど動かないな

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ window.onload = function() {
 //	document.addEventListener("touchmove", TouchEventFunc);
 //	document.addEventListener("touchend", TouchEventFunc);
 	document.getElementById("text-button").addEventListener("touchstart", TouchTextButton);
-	document.getElementById("text-button").addEventListener("touchmove", TouchEventFunc);
+	document.getElementById("text-button").addEventListener("touchmove", testContext);
 //	document.getElementById("text-button").addEventListener("touchend", TouchEventFunc);
 };
 
@@ -38,4 +38,58 @@ function TouchTextButton(e) {
 	const x  = e.touches[0].pageX;
 	const y = e.touches[0].pageY;
 			document.getElementById("text").innerHTML = "X:" + x + ". Y:" + y;
-		} 
+} 
+		
+function testContext() {
+
+	var targetClass = getRuleBySelector('.cvl');
+	var cv = document.getElementById('cvl');	// 要素を得る
+	var ct = cv.getContext('2d');		// 2D(平面)コンテキストを得る
+	ct.fillStyle = '#ffcccc';
+	ct.fillRect(0, 0, cv.width, cv.height);
+}		
+
+// 指定セレクタのCSSルールを取得する
+function getRuleBySelector(sele) {
+	var i, j, sheets, rules, rule = null;
+	
+		// stylesheetのリストを取得
+		sheets = document.styleSheets;
+		for (i=0; i < sheets.length; i++) {
+			// そのstylesheetが持つcssルールのリストを取得
+			rules = sheets[i].cssRules;
+			for(j=0; j < rules.lenth; j++){
+				// セレクタが一致するか調べる
+				if (sele === rules[j].selectorText) {
+					rule = rules[j];
+					break;
+				}
+			}
+		}
+		return rule;
+}
+	
+			
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
